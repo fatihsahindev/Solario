ab474e24cf8b82b282d6eb214ed490d0fb318351
Fatih Şahin <fatihsahin.dev@gmail.com>
2024-01-07 17:59:16 +0300

    dashboard updated.

--- Solution/Client/src/app/modules/dashboard/dashboard.component.ts
+++ Solution/Client/src/app/modules/dashboard/dashboard.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  cash: string = '1.994.281,72';
+  cashRatio: number = 2.76;
+  avgRatio: number = 0.51;
+  cashIncreased: string = this.cashRatio >= 0 ? 'up' : 'down';
+  avgIncreased: string = this.avgRatio >= 1 ? 'up' : 'down';
 
   constructor() { }
 
